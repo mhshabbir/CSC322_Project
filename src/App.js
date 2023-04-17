c22adcbbf601e019a1573ae922745cf73d31569e
elypalacios <elizabethpalacios.cs@gmail.com>
2023-04-16 20:46:07 -0400

    NavBar First Attempt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
-function App() {
+
+function NavBar() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Nav">
+        <h1 className='Logo'>PC PALACE</h1>  
+        <div className='search'>
+        <Box display='flex' 
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 2, width: '50ch', ml:30},
+      }}
+    >
+      <TextField id="outlined-basic" label="Search" variant="outlined"  style={{ backgroundColor: 'white', color: 'black', borderRadius: 30 }} />
+     
+      <Button variant="contained" size="medium" style={{ backgroundColor: 'black', color: 'white' , borderRadius: 30}}> Login</Button>
+   
+  
+    </Box>
+    </div>
     </div>
   );
 }
 
-export default App;
+export default NavBar;
