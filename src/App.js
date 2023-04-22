93eede2656501321983f6da16616c89ccff96083
elypalacios <elizabethpalacios.cs@gmail.com>
2023-04-22 17:24:53 -0400

    Used Browser Routers

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import './App.css';
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import Navbar from './components/navbar';
 
-
-function NavBar() {
+function App() {
   return (
-    <div className="Nav">
-        <h1 className='Logo'>PC PALACE</h1>  
-        <div className='search'>
-        <Box display='flex' 
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 2, width: '50ch', ml:30},
-      }}
-    >
-      <TextField id="outlined-basic" label="Search" variant="outlined"  style={{ backgroundColor: 'white', color: 'black', borderRadius: 30 }} />
-     
-      <Button variant="contained" size="medium" style={{ backgroundColor: 'black', color: 'white' , borderRadius: 30}}> Login</Button>
-   
-  
-    </Box>
-    </div>
-    </div>
+    <Navbar/>
+    
   );
 }
 
-export default NavBar;
+export default App;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
- import { Link } from 'react-router-dom'
- import { ShoppingCart } from 'phosphor-react'
- import "./navbar.css";
+import './navbar.css';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
- export const Navbar = () => {
-   return (
-     <div className='navbar'>
-         <div className='links'>
-             <Link to="/">Shop</Link>
-             <Link to="/cart">
-                 <ShoppingCart size={32}/>
-             </Link>
-         </div>
-     </div>
-   )
- }
+export default function Navbar(){
+  return (
+  <div className="Nav">
+  <h1 className='Logo'>PC PALACE</h1>  
+  <div className='search'>
+  <Box display='flex' 
+component="form"
+sx={{
+  '& > :not(style)': { m: 2, width: '50ch', ml:30},
+}}
+>
+<TextField id="outlined-basic" label="Search" variant="outlined" style={{ backgroundColor: 'white', color: 'black' }} />
+<Button variant="contained" size="medium" style={{ backgroundColor: 'black', color: 'white' }}> Login</Button>
+
+
+</Box>
+</div>
+</div>)
+}
