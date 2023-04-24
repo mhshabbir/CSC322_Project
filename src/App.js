f27dc3d1c2912103753987ec0aaf33cca545ad87
Kenny <kcrosario23@gmail.com>
2023-04-24 02:29:43 -0400

    added link to login button in navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navbar} from "./components/navbar";
-import {Cart} from './pages/cart/cart';
+import { Navbar } from "./components/Navbar";
+import { Cart } from './pages/cart/cart';
 import { Shop } from "./pages/shop/shop";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
+    <div className="wrapper">
+      <h1>
         <Navbar/>
         <Routes>
           <Route path='/' element={<Shop/>} />
           <Route path='/cart' element={<Cart/>} />
           
         </Routes>
-      </Router>
+      </h1>
     </div>
-
   );
 }
 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,24 +1,21 @@
-import './navbar.css';
+import './Navbar.css';
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
-export default function Navbar(){
+ export function Navbar(){
   return (
   <div className="Nav">
-  <h1 className='Logo'>PC PALACE</h1>  
-  <div className='search'>
-  <Box position="relative" display='flex' 
-sx={{
-  '& > :not(style)': { m: 2, width: '50ch', ml:30},
-}}
->
-<TextField  label="Search" variant="outlined" style={{ backgroundColor: 'white', color: 'black', borderRadius: '30px' }} />
-<Button variant="contained" size="medium" style={{ backgroundColor: 'black', color: 'white' , width:'200px',  borderRadius: '30px'}}> Login</Button>
-
-
-</Box>
-</div>
-</div>)
+    <h1 className='Logo'>PC PALACE</h1>  
+    <div className='search'>
+      <Box position="relative" display='flex' sx={{
+      '& > :not(style)': { m: 2, width: '50ch', ml:30},
+      }}>
+        <TextField  label="Search" variant="outlined" style={{ backgroundColor: 'white', color: 'black', borderRadius: '30px' }} />
+          <Button component={Link} to="../Login" variant="contained" size="medium" style={{ backgroundColor: 'black', color: 'white' , width:'200px',  borderRadius: '30px'}}> Login</Button>
+      </Box>
+    </div>
+  </div>)
 }
