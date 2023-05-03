fb1a851d84c8283e31e58ad4a937eb16e1ef9cfc
mhshabbir <shabbir.muhammad@outlook.com>
2023-05-03 17:03:46 -0400

    Correction of errors with liz

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navbar } from "./components/Navbar";
+import { Navbar } from "./components/navbar";
 import { Cart } from './pages/cart/cart';
 import { Shop } from "./pages/shop/shop";
 import { ShopContextProvider } from "./context/shop-context";
