7da34f9d007811d8149251bf77cdcb0f35a5ba83
Muhammad Hasnain Shabbir <83360044+mhshabbir@users.noreply.github.com>
2023-05-07 14:51:32 +0000

    Update login.jsx

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,109 +1,118 @@
 import React, { useState } from "react";
-import { Link, Grid, Box, Button, TextField, Container, Typography, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import {
+  Link,
+  Grid,
+  Box,
+  Button,
+  TextField,
+  Container,
+  Typography,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@mui/material";
 
 export const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [account, setAccount] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [account, setAccount] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, account);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(email, password, account);
-    }
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
 
-    return (
-        <Container component="main" maxWidth="xs">
-            <Box 
-                sx={{
-                    marginTop: 8,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center'
-                }}
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
+          noValidate
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <TextField
+            margin="normal"
+            required
+            name="email"
+            label="Email"
+            id="email-field"
+            placeholder="example@domain.com"
+            autoFocus
+            autoComplete="current-email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <TextField
+            margin="normal"
+            required
+            name="password"
+            label="Password"
+            type="password"
+            id="password-field"
+            placeholder="********"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <FormControl fullWidth>
+            <InputLabel id="account-type">Select Account Type</InputLabel>
+            <Select
+              labelId="account-type-label"
+              id="account-type-select"
+              label="Select Account Type"
+              value={account}
+              onChange={(e) => setAccount(e.target.value)}
             >
+              <MenuItem value={"Owner"}>Owner</MenuItem>
+              <MenuItem value={"Employee"}>Employee</MenuItem>
+              <MenuItem value={"Customer"}>Customer</MenuItem>
+            </Select>
+          </FormControl>
+          <Button
+            type="submit"
+            variant="contained"
+            href="/Login"
+            size="medium"
+            style={{
+              backgroundColor: "black",
+              color: "white",
+              width: "200px",
+              borderRadius: "30px",
+            }}
+          >
+            Login
+          </Button>
 
-                <Typography component="h1" variant="h5">
-                    Login
-                </Typography>
-
-                <Box component="form" 
-                    onSubmit={handleSubmit} 
-                    noValidate 
-                    sx={{   display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                            justifyContent: 'center' }}
-                >
-
-                    <TextField
-                        margin="normal"
-                        required
-                        name="email"
-                        label="Email"
-                        id="email-field"
-                        placeholder="example@domain.com"
-                        autoFocus
-                        autoComplete="current-email"
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                    />
-
-                    <TextField
-                        margin="normal"
-                        required
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password-field"
-                        placeholder="********"
-                        autoComplete="current-password"
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                    />
-
-                    <FormControl fullWidth>
-                        <InputLabel id="account-type">Select Account Type</InputLabel>
-                        <Select
-                            labelId="account-type-label"
-                            id="account-type-select"
-                            label="Select Account Type"
-                            value={account}
-                            onChange={e => setAccount(e.target.value)}
-                        >
-                            <MenuItem value={"Owner"}>Owner</MenuItem>
-                            <MenuItem value={"Employee"}>Employee</MenuItem>
-                            <MenuItem value={"Customer"}>Customer</MenuItem>
-                        </Select>
-                    </FormControl>
-                        <Button
-                            type="submit"
-                            variant="contained" 
-                            href="/Login" 
-                            size="medium" 
-                            style={{ 
-                                backgroundColor: 'black', 
-                                color: 'white' , 
-                                width:'200px',  
-                                borderRadius: '30px'
-                            }}
-                        > 
-                            Login
-                        </Button>
-    
-                        <Grid container>
-                            <Grid item>
-                                <Link href="/SignUp" variant="body2">
-                                    {"Don't have an account? Sign-up"}
-                                </Link>
-                            </Grid>
-
-                        </Grid>
-                    </Grid>
-
-                </Box>
-            </Box> 
-        </Container>
-    )
-}
+          <Grid container>
+            <Grid item>
+              <Link href="/SignUp" variant="body2">
+                {"Don't have an account? Sign-up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
